5e2fbedc090ab88df17f01560ca519185180a166
ashishranacenveo <36648321+ashishranacenveo@users.noreply.github.com>
2018-02-22 13:08:37 -0500

    Delete dbo.ashish1.sql

--- Tables/dbo.ashish1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[ashish1]
-(
-[rana] [int] NULL,
-[test] [char] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[test1] [char] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
